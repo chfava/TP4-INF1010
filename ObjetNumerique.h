dcdb0521885eb18fce648699af58f76dae309dc5
stephanieleclerc <stephanie.leclerc.pll@gmail.com>
2016-03-04 16:40:18 -0500

    Ajout de l'en-tête de fichier

--- ObjetNumerique.h
+++ ObjetNumerique.h
@@ -1,3 +1,10 @@
+/*
+Fichier: ObjetNumerique.h
+Auteur(s): Charles-Olivier Favreau et Stéphanie Leclerc
+Date de creation: 1er mars 2016
+Date de modification: 4 mars 2016
+Description: Définition de la classe ObjetNumerique
+*/
 #ifndef	OBJETNUMERIQUE_H
 #define OBJETNUMERIQUE_H
 
